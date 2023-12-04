be604bf78a2b1fe7cedf732907f863e12e5b77fc
Fanghao Yang <fyang@dexlabs.xyz>
2023-12-04 13:45:17 -0500

    support ITA

--- samplecode/microsoft_azure_attestation/app/src/main.rs
+++ samplecode/microsoft_azure_attestation/app/src/main.rs
@@ -16,7 +16,7 @@
 // under the License..
 
 #![allow(non_snake_case)]
-
+#![allow(deprecated)]
 extern crate base64;
 extern crate base64_url;
 extern crate jsonwebkey;
@@ -41,8 +41,12 @@ use sha2::{Digest, Sha256};
 use x509_certificate::{X509Certificate, X509CertificateError};
 
 static ENCLAVE_FILE: &'static str = "enclave.signed.so";
-const ATTESTATION_PROVIDER_URL: &'static str = "https://sharedeus.eus.attest.azure.net";
-const SGX_ATTESTATION_URI: &'static str = "/attest/SgxEnclave?api-version=2022-08-01";
+const ATTESTATION_PROVIDER_URL: &'static str = "https://api.trustauthority.intel.com";
+const SGX_ATTESTATION_URI: &'static str = "/azure-attestation/attest/SgxEnclave";
+const ITA_CERTS_URL: &'static str =
+    "https://portal.trustauthority.intel.com/azure-attestation/certs";
+const ITA_API_KEY: &'static str = "";
+// const ITA_CERTS: &'static str = r#"{"keys":[{"alg":"PS384","e":"AQAB","kid":"79d80711b754cceb307d4278dc59957f27eb55a8e33d3b824967975843dcbf21df924eebaf93fce186fd291d36817785","kty":"RSA","n":"yE07D7FRSXLsswdeK7h22kw-Xv2K2r4NFoefWElZ6FWmLvCcd27wGEczNeKrE91SWczPtR279tTasQN6_v8qsswC5rCGYlrRWvE0vuPoUXezlV4PX0tCJJJmxWtXFXW0dChWvR1j-_viOItfR8jrybV2-DyVBgGX1ad4BLJJseglPXcofhnKYcG9gp8J2zPFqs1tu6jTW-He3Xw7ZeQNq0n4ZfrRBM3GEYVVsWGlTlqVidMhbvMXSQgz1x2QjyPC2mSUrT-JyA2xTm84Mv_Lmz6FpHXsjXMyPKCUVUf8LSTAiw3UsHa-7QGUW51hh9lZsbWkdSfwGUGxjrcMNEwYo3KvcF8f9Cv1_bkla396poQhtTIHuV478PobzsCfdkbCF5CfwZN31KbqyD9o9pVyzmmQUmOikIZuiSPRnfIU_P8duM5F6yvxQPITZf1RhOPBNYLiOJge7C89OmsM46UKtAYNTieBH-J8oWUUWfAX3pO38bKIzNwHDelSbaeHterJ","x5c":["MIIE/DCCA2SgAwIBAgIBATANBgkqhkiG9w0BAQ0FADBlMQswCQYDVQQGEwJVUzELMAkGA1UECAwCQ0ExGjAYBgNVBAoMEUludGVsIENvcnBvcmF0aW9uMS0wKwYDVQQDDCRJbnRlbCBUcnVzdCBBdXRob3JpdHkgQVRTIFNpZ25pbmcgQ0EwHhcNMjMwOTEyMTExMTUxWhcNMjQwOTExMTExMTUxWjBwMQswCQYDVQQGEwJVUzELMAkGA1UECAwCQ0ExGjAYBgNVBAoMEUludGVsIENvcnBvcmF0aW9uMTgwNgYDVQQDDC9JbnRlbCBUcnVzdCBBdXRob3JpdHkgQXR0ZXN0YXRpb24gVG9rZW4gU2lnbmluZzCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBAMhNOw+xUUly7LMHXiu4dtpMPl79itq+DRaHn1hJWehVpi7wnHdu8BhHMzXiqxPdUlnMz7Udu/bU2rEDev7/KrLMAuawhmJa0VrxNL7j6FF3s5VeD19LQiSSZsVrVxV1tHQoVr0dY/v74jiLX0fI68m1dvg8lQYBl9WneASySbHoJT13KH4ZymHBvYKfCdszxarNbbuo01vh3t18O2XkDatJ+GX60QTNxhGFVbFhpU5alYnTIW7zF0kIM9cdkI8jwtpklK0/icgNsU5vODL/y5s+haR17I1zMjyglFVH/C0kwIsN1LB2vu0BlFudYYfZWbG1pHUn8BlBsY63DDRMGKNyr3BfH/Qr9f25JWt/eqaEIbUyB7leO/D6G87An3ZGwheQn8GTd9Sm6sg/aPaVcs5pkFJjopCGbokj0Z3yFPz/HbjOResr8UDyE2X9UYTjwTWC4jiYHuwvPTprDOOlCrQGDU4ngR/ifKFlFFnwF96Tt/GyiMzcBw3pUm2nh7XqyQIDAQABo4GrMIGoMAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYEFGXRi1GAPlb8fBXUcGQrN5P0nrQAMB8GA1UdIwQYMBaAFCRX9lEHLr6HXZtQaFKogfoiHFl5MAsGA1UdDwQEAwIE8DBLBgNVHR8ERDBCMECgPqA8hjpodHRwczovL3BvcnRhbC50cnVzdGF1dGhvcml0eS5pbnRlbC5jb20vY3JsL2F0cy1jYS1jcmwuZGVyMA0GCSqGSIb3DQEBDQUAA4IBgQCHwDA/kgYi1qZNb7GpO3HpwLP+sjRW2Jm79kgAFORLloW8lPPJab4yl52rNdk2PKl6qsDNwaHrcR4jfe0mxyrdCO2nMtSxJziYK9unwAPF8aCuB4Nym5VNxoVkTvI1r+KMIfBi8eWAREPoH6DgC9bUrqIyXh0zfxTBy3d9rL9+N6OXmP4z6JmzF54ZJnrJJgfKdZ5ClanwDalcLDZSPMTIEZRaagN6PGEUVawCsg+klBBCfgt2j2+Ta3Ri/kCATOmxfRrJSCXYzD2mrcvxHrXIyPT2sRYSNR9nXSwoEH9okfabs0duItQ1fZmKG5j0qAxtQ1vONANpKSIjXSnam89KfaG/xr3x6Z5uq+y7XPSg5gnzn5rLaafuPcSFYthpoX3kYTIUMuO/sqt42x2Ka1sndaAlCEPHfIBys1yBgda0tl+O6d1HVwJ79DBEXqLQftAueGvQSHsIbExkiJvw7Mhdvcid6sb2z8GOY4Ljqj0LHmMi6c1JoyKrkKK9167rW4c=","MIIFCjCCA3KgAwIBAgIBATANBgkqhkiG9w0BAQ0FADB0MSYwJAYDVQQDDB1JbnRlbCBUcnVzdCBBdXRob3JpdHkgUm9vdCBDQTELMAkGA1UEBhMCVVMxCzAJBgNVBAgMAkNBMRQwEgYDVQQHDAtTYW50YSBDbGFyYTEaMBgGA1UECgwRSW50ZWwgQ29ycG9yYXRpb24wHhcNMjMwOTEyMTExMTQ5WhcNMzYxMjMwMTExMTQ5WjBlMQswCQYDVQQGEwJVUzELMAkGA1UECAwCQ0ExGjAYBgNVBAoMEUludGVsIENvcnBvcmF0aW9uMS0wKwYDVQQDDCRJbnRlbCBUcnVzdCBBdXRob3JpdHkgQVRTIFNpZ25pbmcgQ0EwggGiMA0GCSqGSIb3DQEBAQUAA4IBjwAwggGKAoIBgQCof1PJ6PFnZ5TOyxBP8H7kyBxsAopUcMZtJAIdLZV+L+5DVMvH6E/hT4+7XX5SGYkb0R+XquyBz0PayzVrs71k8nL0MrwBIKLuIWQEcmTLC5/18Njf7QxBDZ3+uFrTOcfYcfYpfTNl2v/RvjEM6+KuDhxqLlH//buRO9eALngQQDqHq7pigrB9vVoOtpdng5Az4kjlDOOmdrNNigpEP4u4sQsqcAkSUFbofTXk8OiWtWClL6ItjosedwcabcdXSkAhf/T0QfYCcRQBOhSIblveZbaWgVXKb4S+HlM1Ft/QEtHNpuldlyI/s7+0ISAzVM8vRZU27EuPpQBUseNIrH2+DXTtpop13tozOl64o7VJmB7mwi+Zqv31NT0BucvMUdeC/bg2RSIKlV6RRomUTKtMFo3RpBi3K7+GUMbiq5GNQBNece294wHDhtgA+Bjg59IIxsHx3O9PmTgGAxmx8qAN2e9FPObTNWIHijfue6D0RkbolJd1/BGgAFcmF3pZy+cCAwEAAaOBtTCBsjASBgNVHRMBAf8ECDAGAQH/AgEAMB0GA1UdDgQWBBQkV/ZRBy6+h12bUGhSqIH6IhxZeTAfBgNVHSMEGDAWgBTzCwdViUpG9BjW2nyu+DI+d6gWETAOBgNVHQ8BAf8EBAMCAQYwTAYDVR0fBEUwQzBBoD+gPYY7aHR0cHM6Ly9wb3J0YWwudHJ1c3RhdXRob3JpdHkuaW50ZWwuY29tL2NybC9yb290LWNhLWNybC5kZXIwDQYJKoZIhvcNAQENBQADggGBADtWnJTjnCT6P5GZHS7Qz6MsrpM2IBwWcpnayTe+nV0CAqk4RJay7rupzq8gn8PllPXyWFoComrsT6HPZ80uh0JUIACOmNcO5UhwuRxML+EPmgpVVQJlz68AXf99Y1HaJxJ0aHkFSPr11XUOQ3S657QKee7RJijwcYu6rgfw6eVnYCGr7UD6SSW63D9nZLsa11v8GcIDWPdZVkyPnDVNJulAuWby/FQtZWAs4vCmxWpJYWoy303AVRzEBYoiyBRznWbed0ykyVU6TogLuezoxwH6jrZ7NeaFKrpbnD1YvI3JfP6EzPo1EqjpfumlVW99yY80mrHdr7FpIe9h9RL05utnYcoGt2VzbwN0H3ZXFPBwsBoioLX17xtSM7894w/rHdQV9wEMvxUT2Hmo+rRNu6lCQ3gDsLVXPvBd5rB3tnEY7wYu/uaLvHf01lq9/X9aTuISg63pFsqcb9oCS3hnx//b47/oHjo7yYCPhgKWHJdC5yiiv6U2NqQLeM9FtZIPuQ==","MIIE2TCCA0GgAwIBAgIUZZX2XASGiYPzpZfwGNa7QFlK+3QwDQYJKoZIhvcNAQENBQAwdDEmMCQGA1UEAwwdSW50ZWwgVHJ1c3QgQXV0aG9yaXR5IFJvb3QgQ0ExCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJDQTEUMBIGA1UEBwwLU2FudGEgQ2xhcmExGjAYBgNVBAoMEUludGVsIENvcnBvcmF0aW9uMB4XDTIzMDkxMjExMTE0OFoXDTQ5MTIzMDExMTE0OFowdDEmMCQGA1UEAwwdSW50ZWwgVHJ1c3QgQXV0aG9yaXR5IFJvb3QgQ0ExCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJDQTEUMBIGA1UEBwwLU2FudGEgQ2xhcmExGjAYBgNVBAoMEUludGVsIENvcnBvcmF0aW9uMIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEAuQ1g1Q4MtnvTRhYITKVou5CDvB5FpfjBe8ssUKaDILwkgJlGNv66IWPQ9vhIekZQjuvHbUj9yPcrM7Hva7h7Ehlo/Sm7ZhY8AgZfGFTRJjvNU+vg/BTr5vuqUu/a54eewyzMcmxwmWhK/4cGQs7spFB346jpjSgOHgk5PgJ39PgEr5UL9SvJ1LFRuCNxZTdzyLe7K8cWvEnwGkR2RJpK9pYgzfnAWy8J0djdAKaoQxt8TOE/IwafG/0ujTeuNbzo+3wxeF6SGz56MimE1+KgraPpULaeX2tAL9lUz+ECMetNLbAyqHQwxN1jQZ/3VgpQ8qqh7Cyo4rEjpja29iRtOihBYlW0/X6TxOG1LLSuGo/N9CcSW6EzjsC1Bzakzjs4OD+JGaqqvc255p8URTxZSRSJr1xtimZK+BJoCHECsrCLvCC5UmFfTQrxkeGJ+OIDMkQFgidUw2K7kYXe9k7glKw9yyVf9C0hhqBFfPD8r+CXEA9m2u1tvR4NGMuoegMxAgMBAAGjYzBhMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFPMLB1WJSkb0GNbafK74Mj53qBYRMB8GA1UdIwQYMBaAFPMLB1WJSkb0GNbafK74Mj53qBYRMA4GA1UdDwEB/wQEAwIBBjANBgkqhkiG9w0BAQ0FAAOCAYEALTVDjOJL3XVuK25K6v/hd4z5vZOzhHK6ifnCjLWKu+CkPbKQMMMYMbYTpqm6gEcSr28dPMOF1IWG4xcJX9oJ6b3mDWj3NQNBomBI8fq0hRgz9hKF8wpFpU3UzXtyBLLL18BTCQeJecb6pwrpASVZNmR/fAv8fbR9wWdTBVFHmIrg1wvU1u2WqYSdAgQGke+WdWFIvZCKKMVAB0jX2kPHBQnAkLF3pRaVyNT4I2MCRB8cW2fbSCIARBeryiIHyGCKnDGkDK+dvPxMJ9eMidPbBQBp5t6jxoicg9X8Gw9MeOboOEOB1sIAd0S25V9fzIwwn6j61K4d2VkLf4ZnDa2VKTgmv6NFMynu+JlHVYhQ0yin+dfD63XJvBLcfLrJwK10lsVMX7xv3dB4P+DBYHtWPwrjE28j6+IjCIupuarzDcahBTbRZIAAW4eWKrA3MPQVyGikcdnciFN7kL12EtHhrSBL2jTzsScWvatPqqzIeNxDCywNEyjtGYLnLBnZnbSP"]}]}"#;
 
 extern "C" {
     fn enclave_create_report(
@@ -193,7 +197,7 @@ struct SgxRuntimeData {
 impl SgxRuntimeData {
     fn new_binary(data: &[u8]) -> Self {
         Self {
-            data: base64_url::encode(data),
+            data: base64::encode(data),
             data_type: "Binary".to_string(),
         }
     }
@@ -261,8 +265,11 @@ struct TokenClaims {
 }
 
 /// Validate JsonWebToken with JsonWebKeySet,
-/// only works for RS256 algorithm and token from default attestation provider.
-fn validate_json_web_token(jwt: String, jwks: JsonWebKeySet) -> jwt::errors::Result<TokenClaims> {
+fn validate_json_web_token(
+    jwt: String,
+    jwks: JsonWebKeySet,
+    algo: jwt::Algorithm,
+) -> jwt::errors::Result<TokenClaims> {
     let header = jwt::decode_header(&jwt)?;
     if header.kid.is_none() {
         return Err(jwt::errors::Error::from(
@@ -288,10 +295,9 @@ fn validate_json_web_token(jwt: String, jwks: JsonWebKeySet) -> jwt::errors::Res
     // println!("\n{pem}");
     let key = jwt::DecodingKey::from_rsa_pem(pem.as_bytes())?;
     // prepare validation
-    let algo = jwt::Algorithm::RS256;
     let mut validation = jwt::Validation::new(algo);
     validation.validate_exp = false;
-    validation.iss = Some(ATTESTATION_PROVIDER_URL.to_string());
+    validation.iss = Some("Intel Trust Authority".to_string());
     // decode JWT with the public key
     Ok(jwt::decode::<TokenClaims>(&jwt, &key, &validation)?.claims)
 }
@@ -313,21 +319,22 @@ fn main() {
             "{}{}",
             ATTESTATION_PROVIDER_URL, SGX_ATTESTATION_URI
         ))
+        .header(
+            "X-API-KEY",
+            std::env::var("ITA_API_KEY").unwrap_or_else(|_| ITA_API_KEY.to_string()),
+        )
         .json(&attest_request)
         .send()
         .unwrap();
+    println!("Status: {}", res.status());
     let jwt = res.json::<JwtResponse>().unwrap().token;
     // The below code could be your client-side code to validate the JWT
-    // get public key from azure attestation
-    let res = client
-        .get(format!("{}/certs", ATTESTATION_PROVIDER_URL))
-        .send()
-        .unwrap();
+    let res = client.get(ITA_CERTS_URL).send().unwrap();
     let resp_text = res.text().unwrap();
     // println!("{resp_text}");
     let raw_key_set: RawJsonWebKeySet = serde_json::from_str(&resp_text).unwrap();
     let jwk_set: JsonWebKeySet = raw_key_set.try_into().unwrap();
-    let claims = validate_json_web_token(jwt, jwk_set).unwrap();
+    let claims = validate_json_web_token(jwt, jwk_set, jwt::Algorithm::RS256).unwrap();
     println!(
         "Verified SGX debuggable status: {}",
         claims.x_ms_sgx_is_debuggable
@@ -343,7 +350,7 @@ fn main() {
     println!("Verified SGX SGX SVN: {}", claims.x_ms_sgx_svn);
     println!(
         "Verified SGX runtime data: {}",
-        std::str::from_utf8(&base64_url::decode(&claims.x_ms_sgx_ehd).unwrap()).unwrap()
+        std::str::from_utf8(&base64::decode(&claims.x_ms_sgx_ehd).unwrap()).unwrap()
     );
 }
 
